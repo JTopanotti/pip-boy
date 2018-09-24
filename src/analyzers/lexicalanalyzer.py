7ab2ae2ed3df3d4552715438f29759b0881029d8
tavinho0710 <gustavo-niehues@hotmail.com>
2018-09-24 20:30:43 -0300

    Add TODO item

--- src/analyzers/lexicalanalyzer.py
+++ src/analyzers/lexicalanalyzer.py
@@ -71,6 +71,7 @@ class LexicalAnalyzer:
         self.current_char, self.text = \
             self.text[1], self.text[2:]
 
+    #TODO tratamento para número negativo
     def special_helper(self):
         if self.current_char == '.':
             if self.text and self.text[0] == '.':
